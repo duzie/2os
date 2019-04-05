407b32fd0126227d444332537773e90016828277
fei <1623301>
2019-04-05 20:25:51 +0800

    add toolbox

--- cloud-toolbox/src/main/java/com/f/cloud/toolbox/CloudToolboxApplication.java
+++ cloud-toolbox/src/main/java/com/f/cloud/toolbox/CloudToolboxApplication.java
@@ -3,13 +3,21 @@ package com.f.cloud.toolbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class CloudToolboxApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudToolboxApplication.class, args);
+    }
+
+    @GetMapping
+    public String index(){
+        return "hello";
     }
 
 }
