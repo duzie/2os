fc36052d2c3a5bb2c85987573fc7eb22970d9ad3
fei <1623301>
2019-04-05 20:23:46 +0800

    add toolbox

---
+++ cloud-toolbox/src/main/java/com/f/cloud/toolbox/CloudToolboxApplication.java
@@ -0,0 +1,15 @@
+package com.f.cloud.toolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudToolboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudToolboxApplication.class, args);
+    }
+
+}
